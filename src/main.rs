88345e4119b494211f02532de23c4ef8d595e470
Austin John Mayer (井上 オースティン) <austin.j.mayer@pol.co.jp>
2024-09-25 09:40:06 +0900

    clean up some code

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,8 @@ type ImageMatrix = DMatrix<f64>;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let img = image::open("images/PureMichigan-Background-EbanIceCaves2_0.jpg")?;
 
-    let grayscale = img.grayscale();
-
-    let grayscale_matrix = grayscale
+    let grayscale_matrix = img
+        .grayscale()
         //.resize(400, 400, image::imageops::Nearest)
         .to_luma8();
 
@@ -36,24 +35,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut svd_s_diag = ImageMatrix::zeros(u.nrows(), v_t.ncols());
     svd_s_diag.set_diagonal(&svd.singular_values);
 
+    let mut sleep_time = 500;
+
     for rank in 1..max_rank {
-        std::thread::sleep(Duration::from_millis(200));
+        std::thread::sleep(Duration::from_millis(sleep_time));
 
-        let first_u_col = u.columns(0, rank);
-        let first_s_row = svd_s_diag.view((0, 0), (rank, rank));
-        let first_v_t_col = v_t.rows(0, rank);
+        if sleep_time > 150 {
+            sleep_time -= 5;
+        }
 
-        println!("U~ : {}x{}", first_u_col.nrows(), first_s_row.ncols());
-        println!("SV~ : {}x{}", first_s_row.nrows(), first_s_row.ncols());
-        println!("V'~ : {}x{}", first_v_t_col.nrows(), first_v_t_col.ncols());
+        let u_ncols = u.columns(0, rank);
+        let nxn_sigma = svd_s_diag.view((0, 0), (rank, rank));
+        let v_t_nrows = v_t.rows(0, rank);
 
-        let rank_1 = first_u_col * first_s_row * first_v_t_col;
+        println!("U~ : {}x{}", u_ncols.nrows(), nxn_sigma.ncols());
+        println!("SV~ : {}x{}", nxn_sigma.nrows(), nxn_sigma.ncols());
+        println!("V'~ : {}x{}", v_t_nrows.nrows(), v_t_nrows.ncols());
+
+        let img_approx_matrix = u_ncols * nxn_sigma * v_t_nrows;
 
         println!(
             "SVD Rank {} result {}x{}, original: {}x{}",
             rank,
-            rank_1.row_iter().count(),
-            rank_1.column_iter().count(),
+            img_approx_matrix.row_iter().count(),
+            img_approx_matrix.column_iter().count(),
             grayscale_matrix.height(),
             grayscale_matrix.width()
         );
@@ -62,7 +67,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             image::DynamicImage::new_luma8(grayscale_matrix.width(), grayscale_matrix.height());
 
         for (row_i, pixels) in img_r_1.as_mut_luma8().unwrap().rows_mut().enumerate() {
-            let svd_row = rank_1.row(row_i);
+            let svd_row = img_approx_matrix.row(row_i);
 
             for (col_i, pixel) in pixels.enumerate() {
                 let entry = svd_row.get(col_i).expect("must have entry");
